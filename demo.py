eab393a46a11d0bafd7fd4201019e87830d9745c
Andrey-79 <52529226+Andrey-79@users.noreply.github.com>
2021-05-10 21:12:30 +0400

    Начало тренировки по python
    Самый первый закомитенный вариант

---
+++ demo.py
@@ -0,0 +1,18 @@
+def draw_house(x, y, width, height):
+    """
+    Функция рисует домик ширины width и высоты height
+    от опорной точки  (x, y) которая находиться в нижней
+    точке фундамента
+    :param x координата х середины домика
+    :param y координата у низа фундамента
+    :param width полная ширина домика (фундамент и вылет крыши включены)
+    :param height полная высота домика
+    :return: None
+    """
+    print("Типа рисую домик...", x, y, width, height)
+    pass
+
+x, y = 100, 100
+width, height = 200, 200
+
+draw_house(x, y, width, height)
